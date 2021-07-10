d1db08c96939f031d8db9a6b4bfed0bf6368970c
David Reyes III <dreyesiii@mycwt.com>
2021-07-10 12:26:08 +0800

    added navbar light

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,14 @@
 
+import NavbarTop from "./NavbarTop";
+import NavbarLight from "./NavbarLight";
+
 function App() {
   return (
-    <div class="navbar-top pt-2 pb-2">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <a href="#" class="mb-0 text-white">
-              20% cashback for all users | Code: <strong><span class="text-light">OGOFERS13 <span
-                class="mdi mdi-tag-faces"></span></span> </strong> </a>
-          </div>
-          <div class="col-md-6 text-right">
-            <a href="#" class="text-white"><i aria-hidden="true" class="mdi mdi-map-marker-circle"></i> New York</a>
-            <a href="#" data-target="#bd-example-modal" data-toggle="modal" class="text-white ml-3 mr-3"><i
-              class="mdi mdi-lock"></i> Sign In</a>
-            <a href="#" data-target="#bd-example-modal" data-toggle="modal" class="text-white"><i
-              class="mdi mdi-account-circle"></i> Sign Up</a>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <NavbarTop />
+      <NavbarLight />
+    </>
   );
 }
 
 export default App;
-
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
