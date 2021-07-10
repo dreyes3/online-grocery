d1751a3691dad32a8e1185bdd8bb4b35c307d752
David Reyes III <dreyesiii@mycwt.com>
2021-07-10 11:47:15 +0800

    convert navbar to react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/osahan.css";
+import "./css/owl.carousel.css";
+import "./css/owl.theme.css";
+import "./css/select2-bootstrap.css";
+import "./css/select2.min.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+)
